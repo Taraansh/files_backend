747c323bacb313449acc222ad6dcb6cefc0f4655
Taraansh <taraanshnandwani123@gmail.com>
2024-01-05 18:14:27 +0530

    Added urls to upload and get files

--- user/urls.py
+++ user/urls.py
@@ -11,6 +11,8 @@ urlpatterns = [
     path('signup/', views.signup_for_client, name='signup_for_client'),
     path('signup-operator/', views.signup_for_operator,
          name='signup_for_operator'),
+    path('upload-file/', views.post_file, name="post_file"),
+    path('get-file/', views.get_file, name="get_file"),
     path('login/', views.MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('login/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
